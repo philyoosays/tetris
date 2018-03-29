0ca5c9ec94f8279b61a823a04e0808f20f9d3372
Phil Yoo <philyoo@ool-ad020634.dyn.optonline.net>
2018-03-29 11:44:27 -0400

    thursday morning commit - basic functionality done

--- main.js
+++ main.js
@@ -4,16 +4,38 @@ let xVelocity = 0;
 let borderLeft = false;
 let borderRight = false;
 let objects = [
-  {color: 'orange', xy: [{x: 5, y:1}, {x: 6, y: 1}, {x: 5, y: 2}, {x: 5, y: 3}]},
+  {color: 'orange', xy: [{x: 6, y: 1}, {x: 5, y: 1}, {x: 5, y: 2}, {x: 5, y: 3}]},
+  {color: 'orange', xy: [{x: 5, y: 2}, {x: 5, y: 1}, {x: 4, y: 1}, {x: 3, y: 1}]},
+
+
+
   {color: 'blue', xy: [{x: 4, y: 1}, {x: 5, y: 1}, {x: 5, y: 2}, {x: 5, y: 3}]},
   {color: 'lightblue', xy: [{x: 5, y: 1}, {x: 5, y: 2}, {x: 5, y: 3}, {x: 5, y: 4}]},
   {color: 'yellow', xy: [{x: 4, y: 1}, {x: 5, y: 1}, {x: 4, y: 2}, {x: 5, y: 2}]},
   {color: 'red', xy: [{x: 4, y: 1}, {x: 5, y: 1}, {x: 5, y: 2}, {x: 6, y: 2}]},
-  {color: 'green', xy: [{x: 5, y: 1}, {x: 6, y: 1}, {x: 4, y: 2}, {x: 5, y: 2}]},
+
+  {color: 'green', xy: [{x: 6, y: 1}, {x: 5, y: 1}, {x: 4, y: 2}, {x: 5, y: 2}]},
+  {color: 'green', xy: [{x: 6, y: 1}, {x: 5, y: 1}, {x: 4, y: 2}, {x: 5, y: 2}]},
+
+
+
   {color: 'purple', xy: [{x: 5, y: 1}, {x: 4, y: 2}, {x: 5, y: 2}, {x: 6, y: 2}]},
 ]
+// orange is right L
+// blue is left L
+// lightblue is bar
+// yellow is box
+// red is left S
+// green is right S
+// purple is T
+
 let activeObj = [];
-let activeColor = '';
+let activeDetails = '';
+let gravityTime = 15;
+let gravityCounter = 0;
+let gravity = 1;
+let landEdgeTimer = 0;
+let lineDoneTimer = 0;
 
 function createBoard() {
   for (let y = 0; y < gridHeight + 2; y += 1) {
@@ -24,9 +46,7 @@ function createBoard() {
         theDiv.appendTo('.container');
       } else {
         theDiv.attr('id', `x${x}y${y}`);
-        if (x === 1 || x === 10) {
-          theDiv.addClass('edge');
-        }
+        labelEdges();
         theDiv.addClass('board');
         theDiv.appendTo('.container');
       }
@@ -34,18 +54,166 @@ function createBoard() {
   }
 }
 
-function newObject() {
-  let random = Math.floor(Math.random() * objects.length);
-  activeObj = objects[random].xy;
-  activeColor = objects[random].color;
+function labelEdges() {
+  for (let y = 1; y <= gridHeight; y += 1) {
+    $(`#x1y${y}`).addClass('leftedge');
+    $(`#x10y${y}`).addClass('rightedge');
+  }
 }
 
-// reset velocity after each run so
-// it's not a movement and just and incremented move
+function newObject() {
+  if (activeObj.length === 0) {
+    let random = Math.floor(Math.random() * objects.length);
+    activeObj = objects[random].xy;
+    activeDetails += objects[random].color;
+  }
+}
+
+function drawLandscape() {
+  $('.landedge').removeClass('landedge');
+  for (let x = 1; x <= gridWidth; x += 1) {
+    for (let y = 1; y <= gridHeight; y += 1) {
+      if ($(`#x${x}y${y}`).hasClass('landscape') && !($(`#x${x}y${y-1}`).hasClass('landscape'))) {
+        $(`#x${x}y${y-1}`).addClass('landedge');
+      } else if (y === gridHeight) {
+        $(`#x${x}y${y}`).addClass('landedge');
+      }
+    }
+  }
+}
+
+function placeObj() {
+  for (let i = 0; i < activeObj.length; i += 1) {
+    $(`#x${activeObj[i].x}y${activeObj[i].y}`).addClass('object');
+  }
+}
+
+function objGravity() {
+  if (gravityCounter === gravityTime && activeObj.length > 0) {
+    gravityCounter = 0;
+    activeObj.forEach((v) => {
+      $(`#x${v.x}y${v.y}`).removeClass('object')
+      v.y += gravity;
+    });
+  }
+}
+
+function lateral() {
+  activeObj.forEach((v) => {
+    $(`#x${v.x}y${v.y}`).removeClass('object')
+      v.x += xVelocity;
+  });
+  xVelocity = 0;
+}
+
+function borderDetect() {
+  if ($('.leftedge.object').length === 0) {
+    borderLeft = false;
+  } else if ($('.leftedge.object').length > 0) {
+    borderLeft = true;
+  }
+  if ($('.rightedge.object').length === 0) {
+    borderRight = false;
+  } else if ($('.rightedge.object').length > 0) {
+    borderRight = true;
+  }
+}
+
+function landDetect() {
+  if ($('.landedge.object').length > 0) {
+    gravity = 0;
+    landEdgeTimer += 1;
+    if (landEdgeTimer === 10) {
+      activeObj = [];
+      $('.object').addClass('landscape');
+      $('.object').removeClass('object');
+      landEdgeTimer = 0;
+      gravity = 1;
+    }
+  }
+}
+
+function rotate() {
+  let pivot = activeObj[1];
+  // x' = xCOS90deg - ySINtheta
+  // y' = yCOStheta + xSINtheta
+}
+
+function lineDetect() {
+  for (let y = gridHeight; y >= 1; y -= 1) {
+    let counter = 0;
+    for (let x = 1; x <= gridWidth; x += 1) {
+      if ($(`#x${x}y${y}`).hasClass('landscape')) {
+        counter += 1;
+      }
+    }
+    if (counter === 10) {
+      for (let x = 1; x <= gridWidth; x += 1) {
+        $(`#x${x}y${y}`).addClass('linedone');
+      }
+    }
+  }
+  if ($('.linedone').length > 0) {
+    lineDoneTimer += 1;
+  }
+}
+
+function cleanLineDone() {
+  if (lineDoneTimer !== 10) {
+    lineDoneTimer = 0;
+    $('.linedone').removeClass('landscape');
+    $('.object').removeClass('object');
+    for (let y = gridHeight; y >= 1; y -= 1) {
+      for (let x = 1; x <= gridWidth; x += 1) {
+        if ($(`#x${x}y${y}`).hasClass('linedone')) {
+          $(`#x${x}y${y}`).removeClass('linedone');
+          let clone = $(`#x${x}y${y-1}`).attr('class');
+          $(`#x${x}y${y}`).addClass(clone);
+          $(`#x${x}y${y-1}`).removeClass(clone);
+          $(`#x${x}y${y-1}`).addClass('linedone');
+          $(`#x${x}y${y-1}`).addClass('board');
+          labelEdges()
+        }
+      }
+    }
+    placeObj();
+  }
+}
+
+function generateLand() {
+  for (let y = gridHeight; y >= 12; y -= 1) {
+    for (let x = 1; x <= gridWidth; x += 1) {
+      $(`#x${x}y${y}`).addClass('landscape')
+    }
+  }
+  for(let i = 0; i<10; i++){
+    $(`#x${Math.floor(Math.random()*10)+1}y${Math.floor(Math.random()*9)+12}`).removeClass('landscape');
+  }
+}
+
+function gameOver() {
+
+}
 
  window.onload = function () {
   createBoard();
+  // generateLand();
   $(document).keydown(keyPress);
+  setInterval(game, 1000/30);
+}
+
+function game() {
+  cleanLineDone();
+  drawLandscape();
+  newObject();
+  borderDetect();
+  lateral();
+  objGravity();
+  placeObj();
+  landDetect();
+  lineDetect();
+  console.log(activeObj);
+  gravityCounter++;
 }
 
 function keyPress(button) {
@@ -56,7 +224,7 @@ function keyPress(button) {
       }
       break;
     case 38:
-      // rotation goes here
+      rotate();
       break;
     case 39:
       if (borderRight === false) {
